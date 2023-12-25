e4386b928dac12265d8e4fcf769df0b69a569cfb
Akila Srikantha <110421821+Aksal00@users.noreply.github.com>
2023-12-25 10:44:37 +0530

    LetterA

--- Assets/Scripts/Letter Activity/CoverActive.cs
+++ Assets/Scripts/Letter Activity/CoverActive.cs
@@ -1,24 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
-using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CoverActive : MonoBehaviour
+public class ButtonClickHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
     [SerializeField]
-    private GameObject LetterCoverImage;
-    [SerializeField]
-    private Button StartingButton;
-    [SerializeField]
-    private Button EndingButton;
+    private GameObject targetButton;
 
-    public void setCoverImage()
+    void Update()
     {
-        if (Input.GetButtonDown("StartingButton")){
-
-            LetterCoverImage.SetActive(true);
+        // Check if the left mouse button is pressed
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Check if the mouse click is over the target button
+            if (IsMouseOverButton())
+            {
+                // Handle button click
+                Debug.Log("Button clicked!");
+            }
         }
+    }
+
+    bool IsMouseOverButton()
+    {
+        // Cast a ray from the mouse position
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        // Check if the ray hits a collider
+        if (Physics.Raycast(ray, out hit))
+        {
+            // Check if the collider belongs to the target button
+            if (hit.collider.gameObject == targetButton)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
